3464a5b77b52e4c395ac1450c96defee46922a3d
Mohd Hasan Khan <mhasan4368@gmail.com>
2023-04-09 16:32:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function displayTime(){
+    let dateTime= new Date();
+    let hrs=dateTime.getHours();
+    let min=dateTime.getMinutes();
+    let sec=dateTime.getSeconds();
+    let session=document.getElementById("session");
+
+    if(hrs>=12){
+      session.innerHTML='PM';
+     }
+    else{
+      session.innerHTML='AM';
+    }
+
+    if(hrs>12){     //  for 12 hour formate
+      hrs=hrs-12
+    }
+
+    document.getElementById("hours").innerHTML=hrs;
+    document.getElementById("minutes").innerHTML=min;
+    document.getElementById("seconds").innerHTML=sec;
+    
+    
+  }
+  
+ setInterval(displayTime, 10);
